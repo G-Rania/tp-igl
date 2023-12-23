64fafff2bc0bdd8f42e55078f9a3c45d6c99908e
farah <farahboumaout@gmail.com>
2023-12-24 00:18:41 +0100

    change logo dimention better

--- tp-warafi/src/Pages/Login.jsx
+++ tp-warafi/src/Pages/Login.jsx
@@ -1,70 +1,73 @@
 import React from "react";
-import image from "../assets/Frame8.svg"
-import logo from "../assets/logo.svg"
-import or from "../assets/or.svg"
-import userVector from "../assets/userVector.svg"
-import lockVector from "../assets/lockVector.svg"
+import image from "../assets/Frame8.svg";
+import logo from "../assets/logo.svg";
+import or from "../assets/or.svg";
+import userVector from "../assets/userVector.svg";
+import lockVector from "../assets/lockVector.svg";
 
-export default function Login(){
-  return(
-        <>
-    <div className="  fullPage h-screen w-screen flex flex-row justify-center items-center ">
-           
-        <div className="theImagerow h-full w-[37vw] custom-sm:hidden bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center" style={{ backgroundImage: `url(${image})` }} >                
-            <div className=" theTextDiv h-1/2 w-full flex flex-col justify-end items-center  "> 
-            
-                        <h2 className="text-white font-mada xl:text-4xl sm:text-[24px] font-semibold leading-[37px]  "> Nice to see you again</h2>
-                        <h1 className="text-white font-mada xl:text-6xl sm:text-[30px]  font-bold leading-[37px] mt-[12px] mb-[48px] "> WELCOME BACK</h1>
-                
-            </div>
+export default function Login() {
+  return (
+    <>
+      <div className="fullPage h-screen w-screen flex flex-row justify-center items-center">
+        <div
+          className="theImagerow h-full w-[37vw] custom-sm:hidden bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"
+          style={{ backgroundImage: `url(${image})` }}
+        >
+          <div className="theTextDiv h-1/2 w-full flex flex-col justify-end items-center">
+            <h2 className="text-white font-mada xl:text-4xl sm:text-[24px] font-semibold leading-[37px]">
+              Nice to see you again
+            </h2>
+            <h1 className="text-white font-mada xl:text-6xl sm:text-[30px] font-bold leading-[37px] mt-[12px] mb-[48px]">
+              WELCOME BACK
+            </h1>
+          </div>
 
-            <div className=" signInUpDiv w-full h-1/2 flex flex-col justify-center items-end ">
-                        <button className="relative bg-white text-[#771079] font-gilroy font-bold text-4xl rounded-l-full p-[24px] mb-[24px] overflow-hidden">
-                            <span className="relative z-10">LOGIN</span>
-                            <span className="absolute inset-0 top-0 z-0 bg-gradient-to-b from-gray-300 to-transparent h-4" />
-                        </button>
-                            
-                        <button className="  text-white font-gilroy font-bold text-4xl pr-[24px] mt-[24px]" >SIGNUP</button>
-                
-                </div>
+          <div className="signInUpDiv w-full h-1/2 flex flex-col justify-center items-end">
+            <button className="relative bg-white text-[#771079] font-gilroy font-bold text-4xl rounded-l-full p-[24px] mb-[24px] overflow-hidden">
+              <span className="relative z-10">LOGIN</span>
+              <span className="absolute inset-0 top-0 z-0 bg-gradient-to-b from-gray-300 to-transparent h-4" />
+            </button>
+            <button className="text-white font-gilroy font-bold text-4xl pr-[24px] mt-[24px]">SIGNUP</button>
+          </div>
         </div>
 
-        <div className="secondRow h-full w-[63vw] bg-white flex flex-col justify-center items-center" >
+        <div className="secondRow h-full w-[63vw] bg-white flex flex-col justify-center items-center">
+          <div className="h-60  w-full pt-[40px]  flex items-center justify-center">
+            <img src={logo} alt="Logo" className="w-60 h-60 " />
+          </div>
 
-            <div className="  h-[30%] pt-[40px] w-full flex items-center justify-center " >
-                    <img src={logo} alt="" className=" w-60 h-60"/>
+          <div className="inputUserName h-[40%] w-full flex flex-col items-center justify-center">
+            <div className="flex items-center h-[30%] w-[80%] border-b-2 border-orange-500">
+              <img src={userVector} alt="User Vector" className="h-[20px] w-[20px] mr-[10px]" />
+              <input type="text" placeholder="Username or Email" className="w-[90%] h-full focus:outline-none font-bold text-[#771079]" />
             </div>
-            
-
-            <div className="imputUserName  h-[40%] w-full flex flex-col items-center justify-center ">
-                <div className=" flex items-center h-[20%] w-[80%] border-b-2 border-orange-500 ">
-                    <img src={userVector} alt="" className="h-[20px] w-[20px] mr-[10px] "/>
-                    <input type="text" placeholder="Username or Email" className=" w-[90%] h-full  focus:outline-none font-bold  text-[#771079]" />
-                </div>
-                <div className="flex items-center h-[20%] w-[80%] border-b-2 border-orange-500  " >               
-                     <img src={lockVector} alt="" className="h-[20px] w-[20px] mr-[10px] "/>
-                     <input type="password" placeholder="Enter Password" className="  w-[90%] h-full  focus:outline-none font-bold text-[#771079] " />
-          
-                </div>
-                <div className="hidden custom-sm:flex items-center justify-end  h-[20%] w-full">
-                    <a href="" className="text-[#F87F0F] font-mada font-bold text-[12px]">Forgot Password?</a>
-                </div>
-             </div>
-
-            <div className=" thirdDiv h-[30%] w-[65%] mb-[20px] flex custom-sm:flex-col items-center justify-around   ">
-                <button className="loginButton  pr-[40px] pl-[40px] p-[18px]   rounded-full bg-[#F87F0F] text-white font-gilroy font-extrabold text-2xl   " > LOGIN </button>
-                <a href="" className="text-[#F87F0F] font-mada font-bold text-base custom-sm:hidden ">Forgot Password?</a>
-                <div className=" h-[10%] w-full hidden custom-sm:flex ">
-                    <img src={or} alt="" className=" h-full w-full"/>
-                </div>
-                <button className=" signupButton hidden custom-sm:flex pr-[32px] pl-[32px] p-[18px] rounded-full bg-[#D416D8] text-[#771079]  font-gilroy font-extrabold text-2xl   " > SIGNUP </button>
-               
+            <div className="flex items-center h-[30%] w-[80%] border-b-2 border-orange-500">
+              <img src={lockVector} alt="Lock Vector" className="h-[20px] w-[20px] mr-[10px]" />
+              <input type="password" placeholder="Enter Password" className="w-[90%] h-full focus:outline-none font-bold text-[#771079]" />
             </div>
+            <div className="hidden custom-sm:flex items-center justify-end h-[20%] w-full">
+              <a href="" className="text-[#F87F0F] font-mada font-bold text-[12px]">
+                Forgot Password?
+              </a>
+            </div>
+          </div>
 
-                
+          <div className="thirdDiv h-[30%] w-[65%] mb-[20px] flex custom-sm:flex-col items-center justify-around">
+            <button className="loginButton pr-[40px] pl-[40px] p-[18px] rounded-full bg-[#F87F0F] text-white font-gilroy font-bold text-2xl">
+              LOGIN
+            </button>
+            <a href="" className="text-[#F87F0F] font-mada font-bold text-base custom-sm:hidden">
+              Forgot Password?
+            </a>
+            <div className="h-[10%] w-full hidden custom-sm:flex">
+              <img src={or} alt="Or" className="h-full w-full" />
+            </div>
+            <button className="signupButton hidden custom-sm:flex pr-[32px] pl-[32px] p-[18px] rounded-full bg-[#D416D8] text-[#771079] font-gilroy font-bold text-2xl">
+              SIGNUP
+            </button>
+          </div>
         </div>
-
-     </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
