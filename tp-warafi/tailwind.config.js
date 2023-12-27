f1cab0cbb5aa72507d5d273707f07dc0358e9ece
guerania <lr_gueddouche@esi.dz>
2023-12-27 18:14:36 +0100

    article component

--- tp-warafi/tailwind.config.js
+++ tp-warafi/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {
       width: {
+        '90': '23rem',
         '120': '30rem', 
         '140' : '35rem',
         '150': '37.5rem',
@@ -13,6 +14,9 @@ module.exports = {
       height: {
         '120': '30rem', 
         '150': '37.5rem',
+      },
+      translate:{
+        '7.5' : '1.875rem',
       },
       screens: {
         'custom-sm': {'min': '200px', 'max': '600px'},
