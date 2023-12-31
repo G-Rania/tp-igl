35efa19af9d8aa9e6e286be2390e2e15a622fb1e
guerania <lr_gueddouche@esi.dz>
2023-12-31 12:38:48 +0100

    rien

--- tp-warafi/src/App.js
+++ tp-warafi/src/App.js
@@ -1,8 +1,7 @@
 
 import './App.css'
-import Modview from './Pages/Modview';
 import './index.css'
-/*import {
+import {
   BrowserRouter ,
   Routes,
   Route,
@@ -10,11 +9,11 @@ import './index.css'
 } from "react-router-dom";
 import LandingPage from './Pages/LandingPage.jsx';
 import Signup from './Pages/Sign-up.jsx'
-import Login from './Pages/Login.jsx'*/
+import Login from './Pages/Login.jsx'
 
 function App() {
   return (
-    /*<BrowserRouter>
+    <BrowserRouter>
      <Routes>
         <Route path='/'  element={<LandingPage/>} >
         </Route>
@@ -25,8 +24,7 @@ function App() {
         <Route  path='/signup' element={<Signup/>} >
         </Route>
       </Routes>
-    </BrowserRouter>*/
-   <Modview/> 
+    </BrowserRouter>
     );
 }
 
