e7cd5424bf1497dcdfeaf0ae5889d38f3417f424
guerania <lr_gueddouche@esi.dz>
2024-01-02 12:46:32 +0100

    rien

--- tp-warafi/src/App.js
+++ tp-warafi/src/App.js
@@ -1,45 +1,32 @@
 
 import './App.css'
+
+
 //import Adminbarre from './Components/AdminPage/Adminbarre.jsx';
-//import Modview from './Pages/Modview';
-/*import './index.css'
-import ManageMods from './Pages/ManageMods';
-import UploadFile from './Pages/UploadFile';
-import {
-  BrowserRouter ,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
-import LandingPage from './Pages/LandingPage.jsx';
-import Signup from './Pages/Sign-up.jsx'
-import Login from './Pages/Login.jsx';*/
-//import Article_user from './Components/Userview/Article_user.jsx'
-import FavoritesPage from './Pages/FavoritesPage';
+import Modview from './Pages/Modview';
+import './index.css'
+import LandingPage from './Pages/LandingPage';
+import Login from './Pages/Login';
+import Home from './Pages/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from './Pages/Sign-up.jsx';
+/*import ManageMods from './Pages/ManageMods.jsx';
+import UploadFile from './Pages/UploadFile.jsx';
+import AddModerator from './Components/AdminPage/AddModerator.jsx';*/
 
 function App() {
   return (
-    /*<BrowserRouter>
-     <Routes>
-        <Route path='/'  element={<LandingPage/>} >
-        </Route>
-        
-        <Route path='/login' element={<Login/>}>
-        </Route>
-
-        <Route  path='/signup' element={<Signup/>} >
-        </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/users/login" element={<Login />} />
+        <Route path="/users/home" element={<Home />} />
+        <Route path='/users/signup' element ={<Signup />}></Route>
+        <Route path='/mods' element={<Modview />}></Route>
       </Routes>
-    </BrowserRouter>*/
-   //<UploadFile/>
-   //<ManageMods/>
-   //<Modview/>
-   //<Article_user/>
-   <FavoritesPage/>
-    );
+    </BrowserRouter>  
+  );
+
 }
 
 export default App;
-
-
-
